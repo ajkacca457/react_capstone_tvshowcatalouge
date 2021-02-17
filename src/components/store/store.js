2be1cdcfd95613567efb0a149083e48062831431
Avijit Karmaker <ajkacca@gmail.com>
2021-02-17 09:17:17 +0200

    add store to the project

---
+++ src/components/store/store.js
@@ -0,0 +1,10 @@
+import {createStore , applyMiddleware} from "redux";
+import {composeWithDevTools} from "redux-devtools-extension";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers";
+
+const initialstate={};
+const middleware=[thunk];
+const store= createStore(rootReducer,initialstate,composeWithDevTools(applyMiddleware(...middleware)))
+
+export default store;
